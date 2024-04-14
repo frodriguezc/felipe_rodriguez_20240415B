3a4532a51b170b3b8fbe8c106aa1764b78737b12
SATAN <felipe@carcher.tech>
2024-04-14 17:57:54 -0400

    Initial commit

---
+++ app/src/main/java/tech/carcher/felipe_rodriguez_20240415/MainActivity.kt
@@ -0,0 +1,35 @@
+package tech.carcher.felipe_rodriguez_20240415
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val sportEditText = findViewById<EditText>(R.id.editTextSport)
+        val timeEditText = findViewById<EditText>(R.id.editTextTime)
+        val showButton = findViewById<Button>(R.id.buttonShow)
+
+        showButton.setOnClickListener {
+            val sport = sportEditText.text.toString().trim()
+            val time = timeEditText.text.toString().trim()
+
+            if (sport.isEmpty() || time.isEmpty()) {
+                Toast.makeText(this, "Por favor, complete todos los campos.", Toast.LENGTH_LONG).show()
+            } else {
+                val message = "Deporte: $sport, Tiempo: $time minutos"
+                Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                println("ADVERTENCIA: $message")
+            }
+        }
+    }
+}
